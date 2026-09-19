cf4b9ff7ec70d9b7cd9e0d792a729f1ba7c26b7b
unknown <sourav009966@gmail.com>
2026-09-19 23:07:13 +0530

    first commit

---
+++ tools.py
@@ -0,0 +1,45 @@
+from langchain.tools import tool
+import requests #as we are  have to scrap things from web
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+import os #for file management
+from dotenv import load_dotenv
+from rich import print
+load_dotenv()
+
+
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+
+#tool 1
+@tool
+def web_search(query : str) -> str:
+    """Search the web for recent and reliable information on a topic . Returns Title, URLs and snippets."""
+    results = tavily.search(query = query, max_results= 5)
+
+    out = []
+    for r in results['results']:
+        out.append(
+            f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n"
+        )
+    return "\n-----\n".join(out)
+
+# print(web_search.invoke("What is the recent news of Delhi"))
+
+
+#tool 2
+@tool
+def scrape_url(url:str) -> str:
+    """Scrape and return clean text content from a given URL fpr deeper reading"""
+    try:
+        resp = requests.get(url, timeout=8, headers ={"User-Agent": "Mozilla/5.0 "})
+        # headers ={"User-Agent": "Mozilla/5.0 "} >> this will look like a real user is actually doing login to website, so that website not block our request
+        soup = BeautifulSoup(resp.text, "html.parser") #resp.text will be raw text with html tags
+        for tag in soup(["script", "style", "nav", "footer"]):
+            tag.decompose()
+        return soup.get_text(separator=" ", strip=True)[:3000] #if we would have taken out all then more token will be burn
+    
+    except Exception as e:
+        return f"Could not scrape URL: {str(e)}" 
+
+print(scrape_url.invoke("https://timesofindia.indiatimes.com/city/delhi"))
