21021f54d109e8586e8d2e70db08460b96716602
unknown <sourav009966@gmail.com>
2026-09-20 23:58:02 +0530

    adding agents.py

---
+++ agents.py
@@ -0,0 +1,4 @@
+from langchain.agents import create_agent
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
